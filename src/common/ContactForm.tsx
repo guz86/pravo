2f9e006942907dcb67fb6baf63fba7556dca49f0
Pavel Gordienko <guzmo@yandex.ru>
2024-06-01 18:39:42 +0300

    feat: add header

---
+++ src/common/ContactForm.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './ContactForm.css';
+
+const ContactForm = () => {
+  return (
+    <div className="contact-form-container">
+      <div className="phone-block">
+        <a className="phone-link" href="tel:+79817506666">
+          +7 (931) 590 99 70
+        </a>
+      </div>
+      <div className="order-block">
+        <span className="phone-icon">&#9742;</span>
+        <a className="order-link" href="#">
+          Заказать бесплатный звонок
+        </a>
+      </div>
+      <div className="address-block">
+        Санкт-Петербург, Просвещения пр. 43
+      </div>
+    </div>
+  );
+};
+
+export default ContactForm;
---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import './Header.css';
+import ConsultationButton from '../common/ConsultationButton';
+import Name from '../common/Name';
+import ContactForm from '../common/ContactForm';
+
+const Header: React.FC = () => {
+  return (
+    <Navbar expand="lg" className="bg-dark justify-content-end">
+      <Name />
+      <ConsultationButton />
+      <ContactForm />
+    </Navbar>
+  );
+};
+
+export default Header;
